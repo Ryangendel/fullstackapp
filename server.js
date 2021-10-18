50163f6507403bc99a0c6c3cb896222f000e367d
ryan gendel <ryangendel@ryans-MacBook-Pro.local>
2021-10-18 12:26:08 -0600

    added data to server

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require("express")
 const app = express()
-const PORT = prosses.env.PORT || 3000
+const PORT = process.env.PORT || 3000
+
+const data = [
+    {
+        id:1,
+        name:"Ryan"
+    },
+    {
+        id:2,
+        name:"Nick"
+    },
+    {
+        id:3,
+        name:"Fred"
+    }
+]
 
 app.get("/", (req, res)=>{
-    res.send("helllo")
+    res.send(data)
 })
 
 app.listen(PORT)
